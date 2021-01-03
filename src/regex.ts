ecbc6ea87071cd010fcde43aadc62da2691533fa
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2021-01-02 21:58:48 -0500

    added utils

--- src/regex.ts
+++ src/regex.ts
@@ -1,3 +1,3 @@
-export const BUY_MARKET_ORDER_REGEX_PATTERN = "!BUY (.*) (([1-9])([0-9]*)) MKT"
-export const SELL_MARKET_ORDER_REGEX_PATTERN = "!SELL (.*) (([1-9])([0-9]*)) MKT"
+export const BUY_MARKET_ORDER_REGEX_PATTERN = "^!BUY (.*) (([1-9])([0-9]*)) MKT$"
+export const SELL_MARKET_ORDER_REGEX_PATTERN = "^!SELL (.*) (([1-9])([0-9]*)) MKT$"
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,10 @@
 import { POSITION_TYPES, ORDER_TYPES } from './constants'
+import { BUY_MARKET_ORDER_REGEX_PATTERN } from './regex'
+
+export const isOrder = (message: string, orderRegexPattern: string) => {
+  const regex = new RegExp(orderRegexPattern, 'g');
+  return regex.test(message);
+}
 
 export const isMktOrder = (order: Order) => {
   return order.orderType === ORDER_TYPES.MKT;

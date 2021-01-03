6451727818dc922e59c85b8a3f3ad94caf39852c
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2021-01-02 21:39:59 -0500

    added basic order types

--- src/order.d.ts
+++ src/order.d.ts
@@ -1,7 +1,7 @@
-export interface Order {
+declare interface Order {
   quantity: number,
   ticket: string,
   positionType: string,
-  orderType: string
+  orderType: string,
 }
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,3 @@
-import { Order } from './order'
 import { POSITION_TYPES, ORDER_TYPES } from './constants'
 
 export const isMktOrder = (order: Order) => {

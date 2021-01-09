3b98778992263bd1f64f8453cc6e18e6cf24b53b
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2021-01-09 12:49:23 -0500

    optional mkt

--- src/regex.ts
+++ src/regex.ts
@@ -1,5 +1,5 @@
-export const BUY_MARKET_ORDER_REGEX_PATTERN = "^!BUY (.*) (([1-9])([0-9]*)) (MKT)?$";
-export const SELL_MARKET_ORDER_REGEX_PATTERN = "^!SELL (.*) (([1-9])([0-9]*)) (MKT)?$";
+export const BUY_MARKET_ORDER_REGEX_PATTERN = "^!BUY (.*) (([1-9])([0-9]*))( MKT)?$";
+export const SELL_MARKET_ORDER_REGEX_PATTERN = "^!SELL (.*) (([1-9])([0-9]*))( MKT)?$";
 
 export const REGEX_LIST = [
   BUY_MARKET_ORDER_REGEX_PATTERN,

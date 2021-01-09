5d9eeb9d099461a0bed790226ab930bcd21befad
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2021-01-09 12:58:27 -0500

    add build

--- dist/regex.js
+++ dist/regex.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.REGEX_LIST = exports.SELL_MARKET_ORDER_REGEX_PATTERN = exports.BUY_MARKET_ORDER_REGEX_PATTERN = void 0;
-exports.BUY_MARKET_ORDER_REGEX_PATTERN = "^!BUY (.*) (([1-9])([0-9]*)) MKT$";
-exports.SELL_MARKET_ORDER_REGEX_PATTERN = "^!SELL (.*) (([1-9])([0-9]*)) MKT$";
+exports.BUY_MARKET_ORDER_REGEX_PATTERN = "^!BUY (.*) (([1-9])([0-9]*))( MKT)?$";
+exports.SELL_MARKET_ORDER_REGEX_PATTERN = "^!SELL (.*) (([1-9])([0-9]*))( MKT)?$";
 exports.REGEX_LIST = [
     exports.BUY_MARKET_ORDER_REGEX_PATTERN,
     exports.SELL_MARKET_ORDER_REGEX_PATTERN
